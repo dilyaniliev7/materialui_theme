4e4f18a61fba505c22d95b0a419a53f1675570dc
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-04-14 19:16:36 +0300

    add models in the database and migrate changes

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Themes(models.Model):
+    name = models.CharField(max_length=200)
+    mode = models.CharField(max_length=200)
+    primary_color = models.CharField(max_length=200)
+    secondary_color = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ActiveTheme(models.Model):
+    theme = models.ForeignKey(Themes, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.theme
